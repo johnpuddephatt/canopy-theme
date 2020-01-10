04a8f9cb265296e352543861b98087d8401c14bb
John Puddephatt <john@jdp.org.uk>
2020-01-10 15:07:04 +0000

    change footer order

--- templates/partials/footer.blade.php
+++ templates/partials/footer.blade.php
@@ -2,9 +2,9 @@
   <div class="footer-inner">
     <h2 class="footer-title">Canopy</h2>
     <div class="footer-logos">
+      <img src="@asset('images/ESIF-logo-2.png')" alt="European Structural and Investment Fund logo" class="footer-logo footer-logo-esif" />
       <img src="@asset('images/WHA-logo.png')" alt="World Habitat Awards" class="footer-logo footer-logo-wha" />
       <img src="@asset('images/un-habitat-logo.png')" alt="UN Habitat" class="footer-logo footer-logo-un" />
-      <img src="@asset('images/ESIF-logo-2.png')" alt="European Structural and Investment Fund logo" class="footer-logo footer-logo-esif" />
     </div>
     <nav class="footer-nav">
       @if (has_nav_menu('secondary_navigation'))
