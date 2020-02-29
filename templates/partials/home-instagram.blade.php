8918b8573cb0f8b050961752b2abf126ffabdd26
John Puddephatt <john@jdp.org.uk>
2020-02-29 21:32:08 +0000

    echo instagram shortcode

--- templates/partials/home-instagram.blade.php
+++ templates/partials/home-instagram.blade.php
@@ -1,8 +1,6 @@
 <div class="slide slide-instagram">
   <div class="wrapper">
     <h1 class="slide-title">What we’ve been up to</h1>
-    @php
-      do_shortcode( '[canopyinsta tag="#canopyhome"]' )
-    @endphp
+    {!! do_shortcode( '[canopyinsta tag="#canopyhome"]' ) !!}
   </div>
 </div>
