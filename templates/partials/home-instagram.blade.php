e2ea0f343c51a40b561a0d4487f08ee754ca5a10
John Puddephatt <john@jdp.org.uk>
2021-07-25 14:52:20 +0100

    check instagram shortcode exists

--- templates/partials/home-instagram.blade.php
+++ templates/partials/home-instagram.blade.php
@@ -1,6 +1,8 @@
+@if( shortcode_exists('canopyinsta'))
 <div class="slide slide-instagram">
   <div class="wrapper">
     <h1 class="slide-title">What we’ve been up to</h1>
     {!! do_shortcode( '[canopyinsta tag="#canopyhome"]' ) !!}
   </div>
 </div>
+@endif
