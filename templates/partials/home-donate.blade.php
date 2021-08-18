0e51ed8aa4345e187d891b135d48be4db7ebc3f6
John Puddephatt <john@jdp.org.uk>
2021-08-18 23:51:54 +0100

    donate button

--- templates/partials/home-donate.blade.php
+++ templates/partials/home-donate.blade.php
@@ -7,16 +7,17 @@
     {{-- <div class="image-slider" data-module="donationSlideshow" data-images="donate1,donate2,donate3,donate4"></div> --}}
     <div class="donate--text">
       <h3>Canopy relies on your donations</h3>
-      <input class="ranger" data-module="donationSlider" type="range" min="5" value="10" max="25" value="10" step="5">
+      {{-- <input class="ranger" data-module="donationSlider" type="range" min="5" value="10" max="25" value="10" step="5">
       <h4 class="total">
         Donate
         <span class="wrap">
           £<output>10</output>
         </span>
       </h4>
-      <label><input type="checkbox" value="monthly" name="monthly" class="monthly-check"/>make it monthly</label>
-      <input type="submit" value="Donate" class="donation-submit"/>
-      <p class="small">You’ll be taken to localgiving.org to complete your donation.</p>
+      <label><input type="checkbox" value="monthly" name="monthly" class="monthly-check"/>make it monthly</label> --}}
+      {{-- <input type="submit" value="Donate" class="donation-submit"/> --}}
+      {{-- <p class="small">You’ll be taken to localgiving.org to complete your donation.</p> --}}
+      <a href="/donate" class="button">Donate</a>
 
     </div>
   </div>
